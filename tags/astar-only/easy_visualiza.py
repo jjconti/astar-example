12b6e4eae0a879560805da891df5eea3b46867cd
jjconti <jjconti@3849f24c-4853-0410-924f-80487e21321b>
2008-09-29 19:46:06 +0000

    Tag: astar-only

---
+++ tags/astar-only/easy_visualiza.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+from euclid import LineSegment2, Point2
+from itertools_recipes import pairwise
+
+def flatten(l):
+    r = []
+    for i in l:
+        if isinstance(i, list):
+            r += i
+        else:
+            r.append(i)
+    return r
+
+class Visualiza(object):
+
+    def __init__(self, inicio, elementos):
+        '''
+        elementos -> [inicio, ... puntos, [puntos],... fin]
+        '''
+        elementos = elementos[:]
+        self.puntos = [Point2(float(x),float(y)) for x,y in flatten(elementos) if (x,y) != inicio]
+        self.origen = Point2(float(inicio[0]),float(inicio[1]))
+        self.destinos = self.puntos[:]
+
+        self.poligonos = [self.armar_poligono(e) for e in elementos if isinstance(e, list)]
+        self.segmentos = list(flatten(self.poligonos))
+
+    def armar_poligono(self, puntos):
+        puntos = [Point2(float(x),float(y)) for x,y in puntos]
+        r =[]
+        #FIXME: segmentos redudantes
+        #solo funcionara para figuras convexas
+        for p1 in puntos:
+            for p2 in puntos:
+                if p1 != p2:
+                    r.append(LineSegment2(p1,p2))
+        return r
+
+    def es_visible(self, destino):
+        print self.origen, destino, self.destinos
+        segmento1 = LineSegment2(self.origen, destino)
+        for segmento2 in self.segmentos:
+            r = segmento1.intersect(segmento2)
+            if r and r != self.origen and r != destino:
+                return False
+        return True
+    
+if __name__ == '__main__':
+    '''
+    Salida esperada:
+    Point2(0.00, 4.00) es visible
+    Point2(3.00, 0.00) es visible
+    Point2(3.00, 2.00) es visible
+    Point2(3.00, 4.00) es visible
+    Point2(5.00, 0.00) no es visible
+    Point2(5.00, 4.00) no es visible
+    Point2(7.00, 2.00) no es visible
+    '''
+    elementos = [(0,0), (0,4), [(3,0), (3,2), (3,4), (5,4), (5,0)], (7,2)]
+    inicios = [(0,0), (3,2), (5,4)]
+
+    for inicio in inicios:
+        v = Visualiza(inicio, elementos)
+
+        print "Desde", inicio
+        for destino in v.destinos:
+            print destino, "es visible" if v.es_visible(destino) else "no es visible"
+        print "*"*80
